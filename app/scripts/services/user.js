e39e6902e786ce685d1deb47575967ff4b452794
Francis Marineau <frank.marineau@gmail.com>
2014-06-16 01:44:10 -0400

    FB login start

--- app/scripts/services/user.js
+++ app/scripts/services/user.js
@@ -5,10 +5,6 @@ angular.module('pboardApp')
     return $resource('/api/users/:id', {
       id: '@id'
     }, { //parameters default
-      update: {
-        method: 'PUT',
-        params: {}
-      },
       get: {
         method: 'GET',
         params: {
