decbeff8bedc5021d8d314981ca37800c445f61f
Francis Marineau <frank.marineau@gmail.com>
2014-06-18 01:42:13 -0400

    Slideshow img -> div bg

--- app/scripts/directives.js
+++ app/scripts/directives.js
@@ -29,4 +29,14 @@ angular.module('pboardApp')
 	    	});
 	    }
 	  }
+	})
+
+	.directive('bgImg', function(){
+    return function(scope, element, attrs){
+      attrs.$observe('bgImg', function(value) {
+        element.css({
+          'background-image': 'url(' + value +')'
+        });
+      });
+    };
 	});
