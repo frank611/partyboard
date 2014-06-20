57e167f5a7f69fa53d12c277244ba865e278fec8
Francis Marineau <frank.marineau@gmail.com>
2014-06-20 03:19:48 -0400

    Slideshow images preloading

--- app/scripts/controllers/boardViewing.js
+++ app/scripts/controllers/boardViewing.js
@@ -1,12 +1,23 @@
 'use strict';
 
 angular.module('pboardApp')
-  .controller('BoardViewingCtrl', function ($scope, $routeParams, $interval, Board, $rootScope, socket) {
+  .controller('BoardViewingCtrl', function ($scope, $routeParams, $interval, Board, $rootScope, socket, preloader, $filter) {
+
+  	// Settings
+  	$scope.slideshowSpeed = 3000;
 
   	$scope.newPosts = [];
   	$scope.slideshowIndex = 0;
   	$scope.isLoading = true;
-  	$scope.board = Board.get({ id: $routeParams.id }, function() {$scope.resumeNormalSlideshow()});
+  	$scope.board = Board.get({ id: $routeParams.id }, function() {
+  		// Preload images then start slideshow
+  		$scope.imageLocations = $scope.board.posts.map(function(post) {
+  			return $filter('publicImageUrl')(post);
+  		});
+
+      preloader.preloadImages($scope.imageLocations);
+      $scope.resumeNormalSlideshow();
+  	});
 
 	  socket.on('connect', function() {
 	  	socket.emit('joinBoard', $routeParams.id);
@@ -18,22 +29,24 @@ angular.module('pboardApp')
   	});
 
   	$scope.addNewPostToQueue = function(post) {
-  		$scope.newPosts.push(post);
-  		$interval.cancel($scope.slideshowInterval);
+  		preloader.preloadImages(['http://localhost:9000' + $filter('publicImageUrl')(post)]).then(function() {
+  			$scope.newPosts.push(post);
+	  		$interval.cancel($scope.slideshowInterval);
 
-			function showNewPost() {
-  			if ($scope.newPosts.length > 0) {
-  				$scope.currentPost = $scope.newPosts.shift();
-  				$scope.board.posts.push($scope.currentPost);
-  			}
-  			else {
-  				$interval.cancel($scope.newQueueInterval);
-  				$scope.resumeNormalSlideshow();
-  			}
-  		}
+				function showNewPost() {
+	  			if ($scope.newPosts.length > 0) {
+	  				$scope.currentPost = $scope.newPosts.shift();
+	  				$scope.board.posts.push($scope.currentPost);
+	  			}
+	  			else {
+	  				$interval.cancel($scope.newQueueInterval);
+	  				$scope.resumeNormalSlideshow();
+	  			}
+	  		}
 
-  		$scope.newQueueInterval = $interval(showNewPost, 8000);
-  		showNewPost();
+	  		$scope.newQueueInterval = $interval(showNewPost, $scope.slideshowSpeed);
+	  		showNewPost();
+  		});
   	};
 
   	$scope.resumeNormalSlideshow = function() {
@@ -49,6 +62,6 @@ angular.module('pboardApp')
 	  		else {
 	  			$scope.slideshowIndex = 0;
 	  		}
-	  	}, 8000);
+	  	}, $scope.slideshowSpeed);
   	};
   });
--- app/scripts/controllers/post.js
+++ app/scripts/controllers/post.js
@@ -5,14 +5,65 @@ angular.module('pboardApp')
 
     $scope.uploadPost = function() {
     	var file = document.querySelector('#pictureInput').files[0];
-      S3.uploadImage(file, function(s3url) {
-      	Board.addPost({
-      		id: $routeParams.id
-      	}, {
-      		s3url: s3url,
-      		quote: $scope.quote
-      	});
+
+      $scope.resizeImage(file, function(resizedFile) {
+        S3.uploadImage(resizedFile, function(s3url) {
+          Board.addPost({
+            id: $routeParams.id
+          }, {
+            s3url: s3url,
+            quote: $scope.quote
+          });
+        });
       });
+    };
+
+    $scope.resizeImage = function(file, cb) {
+      function dataURItoBlob(dataURI) {
+        var binary = atob(dataURI.split(',')[1]);
+        var array = [];
+        for(var i = 0; i < binary.length; i++) {
+          array.push(binary.charCodeAt(i));
+        }
+        return new Blob([new Uint8Array(array)], {type: 'image/jpeg'});
+      }
+
+      var img = document.createElement("img");
+      var canvas = document.createElement("canvas");
+      var reader = new FileReader();
+      reader.readAsDataURL(file);
+
+      reader.onload = function(e) { img.src = e.target.result; };
+
+      img.onload = function() {
+        var ctx = canvas.getContext("2d");
+        ctx.drawImage(img, 0, 0);
+
+        var MAX_WIDTH = 1920;
+        var MAX_HEIGHT = 1080;
+        var width = img.width;
+        var height = img.height;
+
+        if (width > height) {
+          if (width > MAX_WIDTH) {
+            height *= MAX_WIDTH / width;
+            width = MAX_WIDTH;
+          }
+        } else {
+          if (height > MAX_HEIGHT) {
+            width *= MAX_HEIGHT / height;
+            height = MAX_HEIGHT;
+          }
+        }
+        canvas.width = width;
+        canvas.height = height;
+        var ctx = canvas.getContext("2d");
+        ctx.drawImage(img, 0, 0, width, height);
+
+        var dataurl = canvas.toDataURL("image/jpeg", 0.5);
+
+        cb(dataURItoBlob(dataurl));
+      };
     };
 
   });
