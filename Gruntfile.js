31fbdd3302db462a2c5ea6e0aba896f0e0f102a4
Francis Marineau <frank.marineau@gmail.com>
2014-06-30 02:29:38 -0400

    master branch for dokku

--- Gruntfile.js
+++ Gruntfile.js
@@ -413,7 +413,7 @@ module.exports = function (grunt) {
       dokku: {
         options: {
           remote: 'dokku@v7apps.com:partyboard',
-          branch: 'prod:master'
+          branch: 'master'
         }
       }
     },
